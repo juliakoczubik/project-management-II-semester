b58cc0782bf602104178e0ea299aa8cfb1b4e61b
AlbenitOoOo <82273766+AlbenitOoOo@users.noreply.github.com>
2023-03-14 11:55:00 +0100

    Poruszanie + Kolizja

---
+++ Gra_2d/Assets/Player.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private BoxCollider2D boxCollider;
+    private Vector3 moveDelta;
+    private RaycastHit2D hit;
+
+    private void Start()
+    {
+        boxCollider = GetComponent<BoxCollider2D>();
+    }
+
+    
+
+    private void FixedUpdate()
+    {
+       
+
+        float x = Input.GetAxisRaw("Horizontal");
+        float y = Input.GetAxisRaw("Vertical");
+
+        //Reset Move Delta
+        
+        moveDelta = new Vector3(x,y,0);
+
+        // Swap sprite direction , wether you're going rigt or lefr
+
+        if (moveDelta.x > 0)
+            transform.localScale = Vector3.one;
+        else if (moveDelta.x < 0)
+            transform.localScale = new Vector3(-1,1,1);
+
+        //Make sure we can move in this direction , by castign a boc there first , if the box rreturns , we're free no move
+
+        hit = Physics2D.BoxCast(transform.position, boxCollider.size, 0, new Vector2(0, moveDelta.y), Mathf.Abs(moveDelta.y * Time.deltaTime), LayerMask.GetMask("Actor", "Blocking"));   
+        if (hit.collider == null)
+        {
+            //Make thing move !
+            transform.Translate(0,moveDelta.y * Time.deltaTime , 0);
+        }
+
+        hit = Physics2D.BoxCast(transform.position, boxCollider.size, 0, new Vector2(moveDelta.x,0), Mathf.Abs(moveDelta.x * Time.deltaTime), LayerMask.GetMask("Actor", "Blocking"));
+        if (hit.collider == null)
+        {
+            //Make thing move !
+            transform.Translate(moveDelta.x * Time.deltaTime, 0 , 0);
+        }
+
+
+
+
+
+
+    }
+
+}
+
