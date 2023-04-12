ae6a99b0aa7cf951c78fc7a62b8637554193de98
Simentis <gamerus123@gmail.com>
2023-04-12 08:40:05 +0200

    Added damage to fights with enemy and push force

--- Gra_2d/Assets/Scripts/Enemy.cs
+++ Gra_2d/Assets/Scripts/Enemy.cs
@@ -1,8 +1,83 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 
-public class Enemy : Fighter
+public class Enemy : Mover
 {
-   
+    //exp
+    public int xpValue = 1;
+
+    //logic
+    public float triggerLenght = 1;
+    public float chaseLenght = 5;
+    private bool chasing;
+    private bool collidingWithPlayer;
+    private Transform playerTransform;
+    private Vector3 startingPostion;
+
+    //hitbox
+    public ContactFilter2D filter;
+    private BoxCollider2D hitbox;
+    private Collider2D[] hits=new Collider2D[10];
+
+    protected override void Start()
+    {
+        base.Start();
+        playerTransform = GameManager.instance.player.transform;
+        startingPostion = transform.position;
+        hitbox= transform.GetChild(0).GetComponent<BoxCollider2D>();    
+    }
+    private void FixedUpdate()
+    {
+        if (Vector3.Distance(playerTransform.position, startingPostion) < chaseLenght)
+        {
+            if (Vector3.Distance(playerTransform.position, startingPostion) < triggerLenght)
+            {
+                chasing = true;
+            }
+            if (chasing)
+            {
+
+                if (!collidingWithPlayer)
+                {
+                    UpdateMotor((playerTransform.position - transform.position).normalized);
+                }
+                else
+                {
+                    UpdateMotor(startingPostion - transform.position);
+                }
+            }
+        }
+        else 
+        {
+            UpdateMotor(startingPostion - transform.position);
+            chasing = false;
+        }
+        //   check for overlaps
+
+        collidingWithPlayer = false;
+
+        boxCollider.OverlapCollider(filter, hits);
+
+        for (int i = 0; i < hits.Length; i++)
+        {
+            if (hits[i] == null)
+                continue;
+
+            if (hits[i].tag == "Fighter" && hits[i].name == "Player") 
+            {
+                collidingWithPlayer = true;
+            }
+
+            hits[i] = null;
+        }
+    }
+    protected override void Death()
+    {
+        Destroy(gameObject);
+        GameManager.instance.experience += xpValue;
+        GameManager.instance.ShowText("+" + xpValue + " xp",30,Color.magenta,transform.position,Vector3.up*40,1.0f);
+
+    }
 }
---
+++ Gra_2d/Assets/Scripts/Mover.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Mover : Fighter
+{
+    protected BoxCollider2D boxCollider;
+    protected Vector3 moveDelta;
+    protected RaycastHit2D hit;
+    protected float ySpeed = 0.75f;
+    protected float xSpeed = 1f;
+    protected virtual void Start()
+    {
+        boxCollider = GetComponent<BoxCollider2D>();
+    }
+
+    protected virtual void UpdateMotor(Vector3 input) {
+        //Reset Move Delta
+
+        moveDelta = new Vector3(input.x*xSpeed,input.y*ySpeed,0);
+
+        // Swap sprite direction , wether you're going rigt or lefr
+
+        if (moveDelta.x > 0)
+            transform.localScale = Vector3.one;
+        else if (moveDelta.x < 0)
+            transform.localScale = new Vector3(-1, 1, 1);
+
+        // Add push vector
+        moveDelta += pushDirection;
+
+        // Reduce push force every frame
+
+        pushDirection = Vector3.Lerp(pushDirection, Vector3.zero, pushRecoverySpeed);
+
+        //Make sure we can move in this direction , by castign a boc there first , if the box rreturns , we're free no move
+
+        hit = Physics2D.BoxCast(transform.position, boxCollider.size, 0, new Vector2(0, moveDelta.y), Mathf.Abs(moveDelta.y * Time.deltaTime), LayerMask.GetMask("Actor", "Blocking"));
+        if (hit.collider == null)
+        {
+            //Make thing move !
+            transform.Translate(0, moveDelta.y * Time.deltaTime, 0);
+        }
+
+        hit = Physics2D.BoxCast(transform.position, boxCollider.size, 0, new Vector2(moveDelta.x, 0), Mathf.Abs(moveDelta.x * Time.deltaTime), LayerMask.GetMask("Actor", "Blocking"));
+        if (hit.collider == null)
+        {
+            //Make thing move !
+            transform.Translate(moveDelta.x * Time.deltaTime, 0, 0);
+        }
+
+
+
+
+
+
+    }
+}
